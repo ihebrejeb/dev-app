83754d4056daa5aa3c1c7f54c486ad3b6279a303
Iheb Rejeb <iheb.rejeb@esprit.tn>
2021-05-03 10:16:27 +0100

    added nginx for load balancing

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,10 @@ mongoose
   .then(() => {
     console.log("connected to db");
   });
+app.get("/", (req, res) => {
+  console.log("wap");
+  res.send("<h1>wap</h1>");
+});
 app.listen(port, () => {
   console.log(`app listening on port: ${3000}`);
 });
